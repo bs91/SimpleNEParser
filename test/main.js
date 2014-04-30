96b7190aeab772793941b4714971b761782c1c8a
Blake Smith <xxblakefailxx@gmail.com>
2014-04-30 12:53:59 -0400

    Initial setup

---
+++ test/main.js
@@ -0,0 +1,9 @@
+var SimpleEmail = require('./SimpleNEParse');
+
+SimpleEmail.fromPath('data.txt', function(email) {
+//  console.log(email);
+});
+
+SimpleEmail.fromString('nowai from string', function(email) {
+//  console.log(email);
+});
