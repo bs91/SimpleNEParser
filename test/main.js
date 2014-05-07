b63ea1965ec04a924832386f8b3bb4aec541f9a4
Blake Smith <xxblakefailxx@gmail.com>
2014-05-07 18:40:47 -0400

    Lots of changes to make the program work minus breaking out to separate files and tests.

--- test/main.js
+++
@@ -1,9 +0,0 @@
-var SimpleEmail = require('../lib/SimpleNEParser');
-
-SimpleEmail.fromPath(__dirname + '/data.txt', function(email) {
-//  console.log(email);
-});
-
-SimpleEmail.fromString('nowai from string', function(email) {
-//  console.log(email);
-});
