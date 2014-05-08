42cbb50984fdea86d115c9ce15c204b54181f18f
Blake Smith <xxblakefailxx@gmail.com>
2014-05-08 15:47:18 -0400

    Missed an import

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 var SimpleEmail = require('../lib/SimpleNEParser');
 
-SimpleEmail.fromPath(__dirname + '/data.txt', function(email) {
-//  console.log(email);
+SimpleEmail.fromPath(__dirname + '/data2.txt', function(email) {
+  console.log(email);
 });
 
 /*SimpleEmail.fromString('nowai from string', function(email) {
